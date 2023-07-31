b4e4f60f714564ec596da48ddb27930e5ffa9f5d
Jaruwat Thongchana <jaruwat.works@gmail.com>
2023-07-31 14:25:42 +0700

    feat: add cors middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 import info from './info'
 import v1 from './v1'
 
 const app = new Hono().basePath('/api')
 
+app.use('*', cors())
 app.route('', info)
 app.route('', v1)
 
